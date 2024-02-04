4793099a12dfd91a7542c7216dcb5d083db8e42d
leonard-liu19 <leonard_liu@foxmail.com>
2024-02-04 22:54:52 +0800

    react quiz获得数据 展示开始页

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,23 +1,47 @@
-import { useReducer } from "react";
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
 
 const initialState = {
 	questions: [],
-	status: "pending",
+	// loading, error, ready, active, finished
+	status: "loading",
 };
 
-function reducer(state, action) {}
+function reducer(state, action) {
+	switch (action.type) {
+		case "dataReceived":
+			return { ...state, questions: action.payload, status: "ready" };
+		case "fetchingFailed":
+			return { ...state, status: "error" };
+		default:
+			throw new Error("Action Unknown");
+	}
+}
 
 export default function App() {
-	const [state, dispatch] = useReducer(reducer, initialState);
+	const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+	const numQuestions = questions.length;
+
+	useEffect(() => {
+		fetch("http://localhost:8000/questions")
+			.then((res) => res.json())
+			.then((data) => dispatch({ type: "dataReceived", payload: data }))
+			.catch((err) => dispatch({ type: "fetchingFailed" }));
+	}, []);
 
 	return (
 		<div className="app">
 			<Header />
 			<Main>
-				<p>1/15</p>
-				<p>Question?</p>
+				{status === "loading" && <Loader />}
+				{status === "error" && <Error />}
+				{status === "ready" && (
+					<StartScreen numQuestions={numQuestions} />
+				)}
 			</Main>
 		</div>
 	);
